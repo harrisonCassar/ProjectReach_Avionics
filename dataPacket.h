432705b4638819a654a366d37b59311c435b8322
harrisonCassar - Desktop <harrison.cassar@gmail.com>
2019-01-19 15:02:25 -0800

    Updated documention and added basic SD functionality in datapacket.h

--- dataPacket.h
+++ dataPacket.h
@@ -29,25 +29,31 @@ struct datapacket {
 //can't do reference in C, so change to pointers
 //can't do reference in C, so change to pointers
 
-bool init(&datapacket dp); {/*...*/}
-//initializes data packet's values to 0..?
+bool init(datapacket& dp); {/*...*/}
+//initializes data packet's values to 0...?
 
-bool print(const &datapacket dp) {/*...*/}
+bool print(const datapacket& dp) {/*...*/}
 //prints all of datapacket struct's data members to output screen
 
-
-
-bool construct(&datapacket dp) {/*...*/}
+bool construct(datapacket& dp) {/*...*/}
 //passing in data packet to construct
 //call "preprocess"ing function on all active sensors, and assign the return values to the data packet's members
+//active sensors can be determined by #define constants, and putting true/false as the value
+//i.e.  #define ACCEL1_IN_USE true
+//....  #define ACCEL2_IN_USE false
+//....  if (ACCEL1_IN_USE) {...}
 
-//accessor for every individual value in datapacket
 
-private:
-  data
+/************************************************OPERATION WITH SD CARD FUNCTIONS*************************************************/
 
-};
+bool writeSD(const datapacket& dp) {/*...*/}
+//returns true if successfully wrote datapacket to SD card through serial connection; returns false if SD card not avaiable (i.e. doesnt have space, or not connected)
+//return type can always be changed to an int to allow for more error returns (i.e. 1 for being out of space, and 2 for being not connected, etc.)
 
-dataPacket hello;
+bool readSD(datapacket& dp) {/*...*/}
+//returns true if successfully wrote datapacket to SD card through serial connection; returns false if SD card not avaiable (i.e. doesnt have space, or not connected)
+//return type can always be changed to an int to allow for more error returns (i.e. 1 for being out of space, and 2 for being not connected, etc.)
+
+/*********************************************************************************************************************************/
 
 #endif
