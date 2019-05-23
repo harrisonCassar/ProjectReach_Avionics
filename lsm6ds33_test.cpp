1dce66296a3c08962b1c654752a7c1edc48dbddd
harrisonCassar - Desktop <harrison.cassar@gmail.com>
2019-05-22 17:39:39 -0700

    Updated simple testing method for LSM6DS33 to work with latest interface (involving update with powerOn and powerOff functionalities)

--- lsm6ds33_test.cpp
+++ lsm6ds33_test.cpp
@@ -6,6 +6,8 @@ int main()
 {
 	LSM6DS33 lsm6(0, 1);
 
+	lsm6.powerOn();
+
 	std::cout << "Input number of desired readings: " << std::endl;
 	unsigned int numOfReadings;
 	std::cin >> numOfReadings;
@@ -15,14 +17,18 @@ int main()
 	for (unsigned int i = 0; i < numOfReadings; i++)
 	{
 		lsm6.poll();
-		lsm6.printRawValues();
+		lsm6.printValues();
 	}
 
-	std::cout << std::endl << std::endl;
-	std::cout << "getBusID: " << lsm6.getBusID() << std::endl;
-	//std::cout << "getStatus: " << lsm6.getStatus() << std::endl;
-	std::cout << "getInstance: " << lsm6.getInstance() << std::endl;
-	//std::cout << "getI2C: " << lsm6.getI2C() << std::endl;
+	lsm6.printSensorInfo();
+
+	// std::cout << std::endl << std::endl;
+	// std::cout << "getBusID: " << lsm6.getBusID() << std::endl;
+	// //std::cout << "getStatus: " << lsm6.getStatus() << std::endl;
+	// std::cout << "getInstance: " << lsm6.getInstance() << std::endl;
+	// //std::cout << "getI2C: " << lsm6.getI2C() << std::endl;
+
+	lsm6.powerOff();
 
 	return 0;
 }
